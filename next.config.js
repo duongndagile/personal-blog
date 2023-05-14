4d951e738b5c2c30550956ee02c8fdd179dda119
duongnd <duong.nd@agiletechsoftware.com>
2023-05-14 21:39:33 +0700

    add Dockerfile

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
-/** @type {import('next').NextConfig} */
+
 const {i18n} = require('./next-i18next.config');
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
   reactStrictMode: true,
   swcMinify: true,
   publicRuntimeConfig: {
     API_URL: process.env.API_URL,
+    GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
   },
-  GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+  output: 'standalone'
 }
 
 module.exports = nextConfig;
